6d3ede1603160f8177f4b5e492e302fe9dc511f4
Ruan Rolim <ruan.rolim.137@gmail.com>
2025-09-07 14:15:07 -0300

    feat: adicionando o filtro de erros

---
+++ src/http-exception/http-exception.filter.ts
@@ -0,0 +1,22 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter<T> implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const message = exception.getResponse()['message'] || exception.message || "Erro não identificado"
+
+    response
+      .status(status)
+      .json({
+        message,
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './http-exception/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe())
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
